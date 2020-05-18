47e1c538428edf931f370610228f4a6bf297a713
MatthewBurtQA <matthewaaronburt@gmail.com>
2020-05-18 19:11:04 +0100

    ProgrammerController of part REST added, took awhile but its there. just got the TeamController now to do

--- src/main/java/com/qa/rest/ProgrammerController.java
+++ src/main/java/com/qa/rest/ProgrammerController.java
@@ -20,8 +20,45 @@ import com.qa.dto.ProgrammerDTO;
 import com.qa.persistence.domain.Programmer;
 import com.qa.service.ProgrammerService;
 
+@RestController
+@RequestMapping("/programmer")
 public class ProgrammerController {
 
+    private ProgrammerService service;
 
+    @Autowired
+    public ProgrammerController(ProgrammerService service){
+        super();
+        this.service = service;
+    }
+    //noow for the rest service calls an their tied functions
+    //starting with Crud
+
+    @PostMapping("/createProgrammer")
+    public ResponseEntity<ProgrammerDTO> createProgrammer(@RequestBody Programmer programmer){
+        return new ResponseEntity<ProgrammerDTO>(this.service.createProgrammer(programmer), HttpStatus.CREATED);
+    }
+    //takes an id, passes for deletion, returns cargo cult programming
+    //cruD
+    @DeleteMapping("/deleteProgrammer/{id}")
+    public ResponseEntity<?>deleteProgrammer(@PathVariable Long id) {
+        return this.service.deleteProgrammer(id) ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build() :ResponseEntity.noContent().build();
+    }
+
+    //cRud
+    @GetMapping("/get/(id)")
+    public ResponseEntity<ProgrammerDTO> getProgrammer(@PathVariable Long id) {
+        return ResponseEntity.ok(this.service.findProgrammerByID(id));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<ProgrammerDTO>> getAllProgrammers(){
+        return ResponseEntity.ok(this.service.readProgrammers());
+    }
+
+    @PutMapping("/updateProgrammer")
+    public ResponseEntity<ProgrammerDTO> updateProgrammer(@PathParam("id") Long id, @RequestBody Programmer programmer) {
+        return new ResponseEntity<ProgrammerDTO>(this.service.updateProgrammer(programmer,id), HttpStatus.ACCEPTED);
+    }
 
 }
